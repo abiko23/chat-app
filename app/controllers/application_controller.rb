d83c1479aaeb432c6ab6b3a84669e6a4e2ab63b2
abiko23 <62099230+abiko23@users.noreply.github.com>
2020-08-25 16:08:06 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user! #参考 https://qiita.com/tobita0000/items/866de191635e6d74e392
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
